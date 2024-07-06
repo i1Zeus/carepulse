ada4f57569780cee9a95eeb52d48ce5547288b58
Hussein Najah <husseinnajah123@gmail.com>
2024-07-06 19:14:26 +0300

    refactor: Update name validation in UserFormValidation

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const UserFormValidation = z.object({
   name: z
     .string()
-    .min(2, "Name must be at least 2 characters")
-    .max(50, "Name must be at most 50 characters"),
+    .min(5, "Name must be at least 5 characters")
+    .max(55, "Name must be at most 55 characters"),
   email: z.string().email("Invalid email address"),
   phone: z
     .string()
