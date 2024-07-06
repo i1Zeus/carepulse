21a52928f6ea69206ef0446cac9ae330977c84e7
Hussein Najah <husseinnajah123@gmail.com>
2024-07-06 18:02:34 +0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="">Home</h1>
-      <Button>Click me!</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* Todo: OTP Verification | PassKeyModal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            height={1000}
+            width={1000}
+            className="w-fit h-10 mb-12"
+          />
+
+          {/* Patient */}
+          <PatientForm />
+
+          <div className="text-14-regular flex justify-between mt-20">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              <span>&copy;</span> 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
