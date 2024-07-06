07d818cc71b3ef92f2ee2be93c14422d3589a5cb
Hussein Najah <husseinnajah123@gmail.com>
2024-07-06 18:44:17 +0300

    refactor: Update placeholder values in PatientForm and add styles for react-phone-number-input

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import ReactDatePicker from "react-datepicker";
 import { Control } from "react-hook-form";
 import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
 
 import { Checkbox } from "./ui/checkbox";
 import {
