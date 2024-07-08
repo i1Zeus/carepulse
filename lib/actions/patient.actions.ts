2e18cefa0051be15c661dc60143d39e3aef27b14
Hussein Najah <husseinnajah123@gmail.com>
2024-07-08 18:54:52 +0300

    patient action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,6 +2,7 @@
 
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -18,5 +19,15 @@ export const createUser = async (user: CreateUserParams) => {
 
       return documents?.users[0];
     }
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (err: any) {
+    console.log(err);
   }
 };
