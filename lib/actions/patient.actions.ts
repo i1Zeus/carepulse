f9de8f1a79b0c31ec571e3444456261eb85a2fbc
Hussein Najah <husseinnajah123@gmail.com>
2024-07-09 13:28:58 +0300

    getPatient

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -42,6 +42,20 @@ export const getUser = async (userId: string) => {
   }
 };
 
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (err: any) {
+    console.log(err);
+  }
+};
+
 export const registerPatient = async ({
   identificationDocument,
   ...patient
@@ -73,5 +87,3 @@ export const registerPatient = async ({
     console.log(error);
   }
 };
-
-export const getPatient = async (userId: string) => {};
