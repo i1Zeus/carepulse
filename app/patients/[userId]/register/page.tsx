628a018116b5fab4d20ca1556465a8c49e069de3
Hussein Najah <husseinnajah123@gmail.com>
2024-07-09 12:55:04 +0300

    auth

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -8,7 +8,8 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   const patient = await getPatient(userId);
 
-  // if (patient) redirect(`/patients/${userId}/new-appointment`);
+  if (patient !== undefined && patient !== null)
+    redirect(`/patients/${userId}/new-appointment`);
 
   return (
     <div className="flex h-screen max-h-screen">
