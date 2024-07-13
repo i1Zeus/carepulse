12f047d9bcfde647b15c3dd36c625193a7bd277e
Hussein Najah <husseinnajah123@gmail.com>
2024-07-13 14:18:04 +0300

    Form

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -125,7 +125,7 @@ export const AppointmentForm = ({
       buttonLabel = "Schedule Appointment";
       break;
     default:
-      buttonLabel = "Submit Apppointment";
+      buttonLabel = "Submit Appointment";
   }
 
   return (
