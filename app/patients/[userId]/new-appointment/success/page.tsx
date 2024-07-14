6f95837f30a23b8f1dca54812c4b04ed1095a0e3
Hussein Najah <husseinnajah123@gmail.com>
2024-07-14 15:03:31 +0300

    user_view_success

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Doctors } from "@/constants";
 import { getAppointment } from "@/lib/actions/appointment.actions";
+import { getUser } from "@/lib/actions/patient.actions";
 import { formatDateTime } from "@/lib/utils";
+
+import * as Sentry from "@sentry/nextjs";
 
 const RequestSuccess = async ({
   searchParams,
@@ -12,6 +15,9 @@ const RequestSuccess = async ({
 }: SearchParamProps) => {
   const appointmentId = (searchParams?.appointmentId as string) || "";
   const appointment = await getAppointment(appointmentId);
+  const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_success", user.name);
 
   const doctor = Doctors.find(
     (doctor) => doctor.name === appointment.primaryPhysician
